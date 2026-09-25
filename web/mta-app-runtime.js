2c059bc367d8bfa874099b97f56e4b9c90bf3f53
galleryabah-source <galleryabah@gmail.com>
2026-09-25 13:41:39 +0700

    chore: refresh operational runtime dashboard cache

--- web/mta-app-runtime.js
+++ web/mta-app-runtime.js
@@ -51,7 +51,7 @@
 
   // Operational runtime is deliberately non-blocking.
   const s=document.createElement('script');
-  s.src='/mta-app-runtime-full.js?v=2';
+  s.src='/mta-app-runtime-full.js?v=3';
   s.async=true;
   s.dataset.mtaOperationalRuntime='1';
   s.onload=()=>console.info('[MTA] operational runtime loaded');
